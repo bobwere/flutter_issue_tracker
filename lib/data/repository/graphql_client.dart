33b0606f411d2938eaf975c5622f9088c146ca53
bob <bob@healthcloud.co.ke>
2021-11-10 12:01:23 +0300

    remove token from codebase

--- lib/data/repository/graphql_client.dart
+++ lib/data/repository/graphql_client.dart
@@ -1,4 +1,5 @@
 import 'package:ferry/ferry.dart';
+import 'package:flutter_issue_tracker/common/constants/keys.dart';
 import 'package:gql_http_link/gql_http_link.dart';
 
 export '../graphql/search.req.gql.dart';
@@ -10,8 +11,6 @@ export '../graphql/labels.var.gql.dart';
 export '../graphql/labels.data.gql.dart';
 
 Client initClient(String url) {
-  const String personal_access_token =
-      "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";
   final link = HttpLink(url,
       defaultHeaders: {"authorization": "Bearer $personal_access_token"});
 
